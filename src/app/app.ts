e276e59b7b07eceb3a87834e682fba21a2eb3d2c
Abdelrahman-Abdelrouf <btranabdo1@gmail.com>
2025-08-01 19:47:55 +0300

    initial commit fot the project

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,9 +1,13 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { Navbar } from './components/navbar/navbar';
+import { Footer } from './components/footer/footer';
+import { Publish } from './components/publish/publish';
+
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, Navbar,Footer,Publish],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
